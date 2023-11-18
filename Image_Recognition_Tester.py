2b9b6d82aca36e33231013f1df3b79f40956ac73
Axeswich <150294995+Axeswich@users.noreply.github.com>
2023-11-18 11:50:03 +0200

    Update Image_Recognition_Tester.py

--- Image_Recognition_Tester.py
+++ Image_Recognition_Tester.py
@@ -1,200 +1,187 @@
-import os
+from flask import Flask, request, render_template, jsonify
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.image import img_to_array
+from PIL import Image, ImageOps
 import numpy as np
-from sklearn.model_selection import train_test_split
-from tensorflow.keras.preprocessing.image import ImageDataGenerator, img_to_array, load_img
-from tensorflow.keras.utils import to_categorical
-from tensorflow.keras.layers import Conv2D, Dense, Flatten, MaxPool2D, Dropout
-from tensorflow.keras.models import Sequential
+import traceback
+import tensorflow as tf
 
-# Defines paths for the training and test datasets.
+# Creates a Flask app.
+# Specifies the template folder as 'template'.
 
-data_dir = 'C:/Users/Stefan/Documents/GTSRB'
-train_path = os.path.join(data_dir, 'Train')
-test_path = os.path.join(data_dir, 'Test')
+app = Flask(__name__, template_folder='template')
 
-# Sets constants such as target image size, number of channels, and the number of categories (traffic sign classes).
+# Defines constants like the path to the pre-trained model, target image size, and normalization factor.
 
+MODEL_PATH = 'C:/Users/Stefan/Documents/GTSRB/trained_model.h5'
 TARGET_SIZE = (30, 30)
-IMG_HEIGHT, IMG_WIDTH = TARGET_SIZE
-CHANNELS = 3
-NUM_CATEGORIES = len(os.listdir(train_path))
+NORMALIZATION_FACTOR = 255.0
 
-# Defines a dictionary classes mapping class indices to traffic sign descriptions.
+# Defines a custom session and graph for TensorFlow to manage the model loading and prediction.
 
-classes = { 0: 'Limita de viteza (20km/h)',
-            1: 'Limita de viteza (30km/h)',
-            2: 'Limita de viteza (50km/h)',
-            3: 'Limita de viteza (60km/h)',
-            4: 'Limita de viteza (70km/h)',
-            5: 'Limita de viteza (80km/h)',
-            6: 'Sfarsitul limitei de viteza (80km/h)',
-            7: 'Limita de viteza (100km/h)',
-            8: 'Limita de viteza (120km/h)',
-            9: 'Nu se depaseste',
-            10: 'Vehiculele peste 3.5 tone nu au voie',
-            11: 'Prioritate dreapta la intersectie',
-            12: 'Drum cu prioritate',
-            13: 'Cedeaza trecerea',
-            14: 'Stop',
-            15: 'Vehiculele nu au voie',
-            16: 'Vehiculele peste 3.5 tone nu au voie',
-            17: 'Nu e voie sa intri',
-            18: 'Atentie',
-            19: 'Curba stanga periculoasa',
-            20: 'Curba dreapta periculoasa',
-            21: 'Curba dubla',
-            22: 'Drum accidentat',
-            23: 'Drum alunecos',
-            24: 'Drumul se ingusteaza spre dreapta',
-            25: 'Se lucreaza',
-            26: 'Indicatoare rutiere',
-            27: 'Trecere pietoni',
-            28: 'Trecere copii',
-            29: 'Trecere biciclete',
-            30: 'Atentie la gheata/zapada',
-            31: 'Trecere animale salbatice',
-            32: 'End speed + passing limits',
-            33: 'Vireaza dreapta',
-            34: 'Vireaza stanga',
-            35: 'Sens unic',
-            36: 'Mergi in fata sau dreapta',
-            37: 'Mergi in fata sau stanga',
-            38: 'Pastreaza partea dreapta',
-            39: 'Pastreaza partea stanga',
-            40: 'Sens giratoriu mandatoriu',
-            41: 'De acum se poate depasii',
-            42: 'De acum pot depasii vehiculele de peste 3.5 tone'}
+model_session = tf.compat.v1.Session()
+model_graph = tf.compat.v1.get_default_graph()
 
-# Counts the number of images in each class and sorts the classes based on the number of images.
+# Loads the pre-trained traffic sign recognition model within the custom TensorFlow session and graph.
 
-folders = os.listdir(train_path)
+def load_traffic_sign_model():
+    global model_session
+    global model_graph
 
-train_number = []
-class_num = []
+    with model_graph.as_default():
+        with model_session.as_default():
+            model = load_model(MODEL_PATH)
 
-for index, folder in enumerate(folders):
-    train_files = os.listdir(os.path.join(train_path, folder))
-    train_number.append(len(train_files))
-    class_num.append(classes[index])
+    return model
 
-# Sorting the dataset on the basis of number of images in each class
-zipped_lists = zip(train_number, class_num)
-sorted_pairs = sorted(zipped_lists)
-tuples = zip(*sorted_pairs)
-train_number, class_num = [list(tuple) for tuple in tuples]
+# Defines a function to be executed before the first request.
+# Clears the default session and sets the custom session as default.
+# Loads the traffic sign model and stores it in the Flask app object.
 
-# Defines a function load_data to load images and labels from the dataset.
+@app.before_first_request
+def before_first_request():
+    global model_session
+    global model_graph
+
+    # Clear the default session and set the custom session as default
+    tf.compat.v1.keras.backend.clear_session()
+    tf.compat.v1.keras.backend.set_session(model_session)
+
+    # Load the traffic sign model and store it in the app object
+    app.model = load_traffic_sign_model()
+
+# Defines a function to preprocess an image.
+# Resizes the image, converts it to RGB if not in 'RGB' mode, converts it to grayscale if not in 'L' mode,
+# and applies colorization if the image is in 'L' mode.
 
 
-def load_data(data_dir):
-    images = list()
-    labels = list()
-    for category in range(NUM_CATEGORIES):
-        categories = os.path.join(data_dir, str(category))
-        for img in os.listdir(categories):
-            img = load_img(os.path.join(categories, img), target_size=TARGET_SIZE)
-            image = img_to_array(img)
-            images.append(image)
-            labels.append(category)
+def preprocess_image(image):
+    # Resize image to target size
+    image = image.resize(TARGET_SIZE)
 
-    return images, labels
+    # Convert to RGB if not in 'RGB' mode
+    if image.mode != 'RGB':
+        image = image.convert('RGB')
 
-# Calls load_data to load images and labels from the training dataset.
+    # Convert to grayscale if not in 'L' mode
+    if image.mode != 'L':
+        image = ImageOps.grayscale(image)
+
+    # Apply colorize only if the image is in 'L' mode
+    if image.mode == 'L':
+        image = ImageOps.colorize(image, 'black', 'white').convert('RGB')
+
+    return img_to_array(image)
+
+# Defines a function to predict the traffic sign class given an image array.
+# Uses the pre-trained model for prediction.
 
 
-images, labels = load_data(train_path)
+def predict_traffic_sign(image_array):
+    global model_session
+    global model_graph
 
-# Performs one-hot encoding on the labels.
-labels = to_categorical(labels)
+    with model_graph.as_default():
+        with model_session.as_default():
+            image_array = np.expand_dims(image_array, axis=0)
+            image_array /= NORMALIZATION_FACTOR
 
-# Splits the dataset into training and testing sets using train_test_split.
+            # Use the loaded model for prediction
+            prediction = app.model.predict(image_array)
+            predicted_class = np.argmax(prediction, axis=1)
 
-x_train, x_test, y_train, y_test = train_test_split(
-                                                    np.array(images),
-                                                    labels,
-                                                    test_size=0.3,
-                                                    random_state=42,
-                                                    shuffle=True
-                                                    )
+    return predicted_class[0]
 
-x_train = x_train/255
-x_test = x_test/255
-print("X_train.shape", x_train.shape)
-print("X_valid.shape", x_test.shape)
-print("y_train.shape", y_train.shape)
-print("y_valid.shape", y_test.shape)
+# Defines a function to format the predicted class index into a human-readable traffic sign description.
 
-# Defines a Sequential model.
-# Adds convol layers with activation functions (ReLU),max-pooling layers,and dropout layers to prevent overfitting.
-# Adds fully connected (dense) layers with ReLU activation.
-# Uses softmax activation in the output layer with 43 units (number of traffic sign classes).
 
-model = Sequential()
-model.add(Conv2D(filters=16, kernel_size=(3, 3), activation="relu", input_shape=(IMG_HEIGHT, IMG_WIDTH, 3)))
-model.add(Conv2D(filters=32, kernel_size=(3, 3), activation="relu"))
-model.add(MaxPool2D(pool_size=(2, 2)))
-model.add(Dropout(rate=0.25))
-model.add(Conv2D(filters=64, kernel_size=(3, 3), activation="relu"))
-model.add(Conv2D(filters=128, kernel_size=(3, 3), activation="relu"))
-model.add(MaxPool2D(pool_size=(2, 2)))
-model.add(Dropout(rate=0.25))
-model.add(Flatten())
-model.add(Dense(512, activation="relu"))
-model.add(Dropout(rate=0.5))
-model.add(Dense(43, activation="softmax"))
+def format_prediction(class_index):
+    classes = {0: 'Limita de viteza (20km/h)',
+               1: 'Limita de viteza (30km/h)',
+               2: 'Limita de viteza (50km/h)',
+               3: 'Limita de viteza (60km/h)',
+               4: 'Limita de viteza (70km/h)',
+               5: 'Limita de viteza (80km/h)',
+               6: 'Sfarsitul limitei de viteza (80km/h)',
+               7: 'Limita de viteza (100km/h)',
+               8: 'Limita de viteza (120km/h)',
+               9: 'Nu se depaseste',
+               10: 'Vehiculele peste 3.5 tone nu au voie',
+               11: 'Prioritate dreapta la intersectie',
+               12: 'Drum cu prioritate',
+               13: 'Cedeaza trecerea',
+               14: 'Stop',
+               15: 'Vehiculele nu au voie',
+               16: 'Vehiculele peste 3.5 tone nu au voie',
+               17: 'Nu e voie sa intri',
+               18: 'Atentie',
+               19: 'Curba stanga periculoasa',
+               20: 'Curba dreapta periculoasa',
+               21: 'Curba dubla',
+               22: 'Drum accidentat',
+               23: 'Drum alunecos',
+               24: 'Drumul se ingusteaza spre dreapta',
+               25: 'Se lucreaza',
+               26: 'Indicatoare rutiere',
+               27: 'Trecere pietoni',
+               28: 'Trecere copii',
+               29: 'Trecere biciclete',
+               30: 'Atentie la gheata/zapada',
+               31: 'Trecere animale salbatice',
+               32: 'End speed + passing limits',
+               33: 'Vireaza dreapta',
+               34: 'Vireaza stanga',
+               35: 'Sens unic',
+               36: 'Mergi in fata sau dreapta',
+               37: 'Mergi in fata sau stanga',
+               38: 'Pastreaza partea dreapta',
+               39: 'Pastreaza partea stanga',
+               40: 'Sens giratoriu mandatoriu',
+               41: 'De acum se poate depasii',
+               42: 'De acum pot depasii vehiculele de peste 3.5 tone'}
+    return classes[class_index]
 
-model.summary()
+# '/': Renders the upload HTML page (index.html).
+# '/predict': Handles POST requests for traffic sign prediction.
+# Accepts an uploaded image file.
+# Preprocesses the image, predicts the traffic sign class, formats the prediction, and returns a JSON response.
 
-# Compiles the model using categorical crossentropy loss, the Adam optimizer, and accuracy as the metric.
+@app.route('/', methods=['GET'])
+def index():
+    # Render the upload HTML page
+    return render_template("index.html")
 
-model.compile(
-    loss='categorical_crossentropy',
-    optimizer='adam',
-    metrics=['accuracy']
-)
+@app.route('/predict', methods=['POST'])
+def predict():
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part'})
 
-# Defines an image data generator (aug) for data augmentation during training.
+    file = request.files['file']
 
-aug = ImageDataGenerator(
-    rotation_range=10,
-    zoom_range=0.15,
-    width_shift_range=0.1,
-    height_shift_range=0.1,
-    shear_range=0.15,
-    horizontal_flip=False,
-    vertical_flip=False,
-    fill_mode="nearest")
+    if file.filename == '':
+        return jsonify({'error': 'No selected file'})
 
-# Another generator (aug_validation) is used for the validation data.
+    try:
+        image = Image.open(file)
+        print(f"Original image mode: {image.mode}, shape: {np.array(image).shape}")
 
-aug_validation = ImageDataGenerator()
+        # Preprocess the image
+        image_array = preprocess_image(image)
+        print(f"Processed image shape: {image_array.shape}")
 
-model_save_path = os.path.join(data_dir, 'trained_model.h5')
+        # Predict the traffic sign
+        predicted_class = predict_traffic_sign(image_array)
 
-EPOCHS = 30
-history = model.fit(
-    aug.flow(x_train, y_train, batch_size=32),
-    validation_data=aug_validation.flow(x_test, y_test),
-    epochs=EPOCHS
-)
+        # Format the prediction result
+        prediction_result = format_prediction(predicted_class)
 
-# Saves the trained model to a file (trained_model.h5).
+        return jsonify({'prediction': prediction_result})
 
-model.save(model_save_path)
-print(f"Model saved to {model_save_path}")
+    except Exception as e:
+        traceback.print_exc()
+        return jsonify({'error': str(e)})
 
-# Evaluates the model on the test set and prints the accuracy.
+# Runs the Flask app in debug mode if the script is executed directly.
 
-loss, accuracy = model.evaluate(x_test, y_test)
-
-print('test set accuracy: ', accuracy * 100)
-
-# Plots the training and validation accuracy and loss over epochs.
-
-acc = history.history['acc']
-val_acc = history.history['val_acc']
-
-loss = history.history['loss']
-val_loss = history.history['val_loss']
-
-epochs_range = range(EPOCHS)
+if __name__ == '__main__':
+    app.run(debug=True)
